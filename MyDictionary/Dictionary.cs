99033693d011bddad1509dc4ca53feb89ff9d415
ezgi özgen <ozgenn.ezgi@gmail.com>
2021-01-21 20:35:47 +0300

    Proje dosyası ekle.

---
+++ MyDictionary/Dictionary.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class Dictionary<T, Tvalue>
+    {
+        T[] akey;
+        Tvalue[] avalue;
+        
+        public Dictionary()
+        {
+            akey = new T[0];
+            avalue = new Tvalue[0];
+            
+        }
+
+        public void Add(T key, Tvalue value)
+        {
+            T[] tempKey = akey;
+            Tvalue[] tempValue = avalue;
+           
+
+            akey = new T[akey.Length + 1];
+            avalue = new Tvalue[avalue.Length + 1];
+            
+
+
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                akey[i] = tempKey[i];
+                avalue[i] = tempValue[i];
+               
+
+            }
+
+            akey[akey.Length - 1] = key;
+            avalue[avalue.Length - 1] = value;
+            
+
+        }
+
+        public T[] Key
+        {
+            get { return akey; }
+        }
+
+        public Tvalue[] Value
+        {
+            get { return avalue; }
+        }
+
+        
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Products in stock");
+            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+            dictionary.Add("Ürün1", "Apple");
+            dictionary.Add("Ürün2", "Watermelon");
+            dictionary.Add("Ürün3", "Banana");
+            dictionary.Add("Ürün4", "Orange");
+
+
+
+            foreach (var value in dictionary.Value)
+            {
+                Console.WriteLine(value);
+            }
+
+        }
+    }
+}
